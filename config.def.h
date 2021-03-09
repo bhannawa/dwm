be24715bfc7a75921523bf8cacd1415b082b7f62
FACKDAWURLD <brandonhannawa@gmail.com>
2021-03-09 16:18:25 -0500

    Played with fonts

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@ static const unsigned int minwsz    = 20;       /* Minimal heigt of a client for
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "hack:size=10", "JoyPixels:pixelsize=10" };
-static const char dmenufont[]       = "hack:size=10" ;
+static const char *fonts[]          = { "mono:size=10", "JoyPixels:pixelsize=10" };
+static const char dmenufont[]       = "mono:size=10" ;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
